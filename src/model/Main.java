56d4a5737de58b3e8c1bf8251e2e87b8edba41fd
CamiloCJ09 <camilocampazj@gmail.com>
2021-03-02 15:59:26 -0500

    Reading name

--- src/model/Main.java
+++ src/model/Main.java
@@ -3,9 +3,10 @@ import  java.util.Scanner;
 public class Main {
     private  Scanner sc;
     public Main(){
-        sc = new Scanner(System.in);
+
     }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Pelele");
 
         System.out.println("El viejo campaz");
@@ -14,5 +15,7 @@ public class Main {
         System.out.println("peleleX2");
         System.out.println("hey");
 
+        String name = sc.nextLine();
+        System.out.println(name);
     }
 }
